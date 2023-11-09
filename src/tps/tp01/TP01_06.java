e22914bd2dfb26a444e7679a2d286b24369fdac2
rei-rala <ramonirala@outlook.com>
2023-11-08 23:52:04 -0300

    tp01_06 - cola con prioridad

--- src/tps/tp01/TP01_06.java
+++ src/tps/tp01/TP01_06.java
@@ -1,5 +1,53 @@
 package tps.tp01;
 
-public class TP01_06 {
+import ownimple.OwnCola;
+import ownimple.OwnColaPrioridad;
+import owntda.OwnColaPrioridadTDA;
+import owntda.OwnColaTDA;
+//import tda.ColaPrioridadTDA;
 
+public class TP01_06 {
+	public void pasarColaConPrioridad(OwnColaPrioridadTDA origen, OwnColaPrioridadTDA destino) {
+		while (!origen.colaVacia()) {
+			destino.acolarPrioridad(origen.primero(), origen.prioridad());
+			origen.desacolar();
+		}
+	}
+	
+	public static OwnColaPrioridadTDA combinarColasPrioridad(OwnColaPrioridadTDA cp1, OwnColaPrioridadTDA cp2) {
+		OwnColaPrioridadTDA destino = new OwnColaPrioridad();
+		destino.inicializarCola();
+		
+		// Dada la comparacion al acolar (prioridad>=)
+		// Se acola primero los valores de cp2		
+		while (!cp1.colaVacia()) {
+			destino.acolarPrioridad(cp1.primero(), cp1.prioridad());
+			cp1.desacolar();
+		}
+		
+		while (!cp2.colaVacia()) {
+			destino.acolarPrioridad(cp2.primero(), cp2.prioridad());
+			cp2.desacolar();
+		}
+		
+		return destino;
+	}
+	
+	public static OwnColaPrioridadTDA pasarColaPrioridadHacia2Colas(OwnColaPrioridadTDA origen) {
+		OwnColaPrioridadTDA resultado = new OwnColaPrioridad();
+		
+		OwnColaTDA valores = new OwnCola();
+		valores.inicializarCola();
+		
+		OwnColaPrioridadTDA CPaux = new OwnColaPrioridad();
+		CPaux.inicializarCola();
+		
+		while (!origen.colaVacia()) {
+			
+			
+			origen.desacolar();
+		}
+		
+		return resultado;
+	}
 }
